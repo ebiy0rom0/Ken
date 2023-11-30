02bec93056ee58ba51a6f8b8bae659d01f4fca77
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-11-30 23:23:30 +0900

    [mod]holds fixed instances

--- src/structures/types/honami.ts
+++ src/structures/types/honami.ts
@@ -1,14 +1,18 @@
 import { Bot, Collection } from "../../deps.ts";
 import { Reminder } from "../../utils/mod.ts";
 import { ReminderType } from "../../utils/reminder.ts";
+import { ShiftRecruiter } from "../../utils/shiftRecruiter.ts";
+import { Channel } from "../discord/channel.ts";
+import { Guild } from "../discord/guild.ts";
 import { ChatInputInteractionCommand } from "./command.ts";
 
 export interface Honami extends Bot {
   kv: Deno.Kv
-  allowedGuildID: bigint
+  guild: Guild
+  botChannel: Channel
   roomChannelID: bigint
   listenOnlyChannelID: bigint
-  botInfoChannelID: bigint
   commands: Collection<string, ChatInputInteractionCommand>
   reminders: Collection<ReminderType, Reminder>
+  recruiter: ShiftRecruiter
 }
