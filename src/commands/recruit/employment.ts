c69ef4b81d3152dd976be8273998e6c07bdc8e5f
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-11-30 23:24:52 +0900

    [mod]operate recruiter

--- src/commands/recruit/employment.ts
+++ src/commands/recruit/employment.ts
@@ -1,8 +1,9 @@
+import { honami } from "../../client/honami.ts";
 import { createCommand } from "../../utils/mod.ts"
 
 export default createCommand({
   name: "employment",
   description: "Shift applications will be closed.",
 
-  execute: async _ => await 1
+  execute: async _ctx => await honami.recruiter.stop()
 })
--- src/commands/recruit/recruit.ts
+++ src/commands/recruit/recruit.ts
@@ -1,8 +1,9 @@
+import { honami } from "../../client/honami.ts";
 import { createCommand } from "../../utils/mod.ts"
 
 export default createCommand({
   name: "recruit",
   description: "Start the shift recruitment. Performed on a regular daily basis.",
 
-  execute: async _ => await 1
+  execute: async _ctx => await honami.recruiter.start()
 })
