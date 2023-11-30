89a79781d1a0305c7c5e6ec56efde4343ff66a01
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-01 02:06:08 +0900

    [add]boilerplate base

---
+++ src/config/messages.ts
@@ -0,0 +1,14 @@
+
+export const Messages = {
+  Error: {
+
+  },
+  Bonus: {
+    Start: "りはびり、しっかり炊けよ",
+    Stop: "今日はここまでだな"
+  },
+  Recruit: {
+    Start: "シフトの募集を開始する",
+    Stop: "シフトの募集を終了する"
+  }
+}
