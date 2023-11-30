959eb77ae5f298c7be7af11328e518cf22ff29db
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-01 01:51:18 +0900

    [add]reminde interval option
    and fix respond message

--- src/commands/bonus/burn.ts
+++ src/commands/bonus/burn.ts
@@ -1,4 +1,6 @@
 import { honami } from "../../client/honami.ts";
+import { Messages } from "../../config/messages.ts";
+import { ApplicationCommandOptionTypes } from "../../deps.ts";
 import { Channel } from "../../structures/discord/channel.ts";
 import { createCommand } from "../../utils/mod.ts"
 import { Reminder, ReminderTypes } from "../../utils/reminder.ts";
@@ -6,17 +8,28 @@ import { Reminder, ReminderTypes } from "../../utils/reminder.ts";
 export default createCommand({
   name: "burn",
   description: "Regularly urge runner to burn live-bonuses.",
+  options: [
+    {
+      type: ApplicationCommandOptionTypes.Integer,
+      name: "interval",
+      description: "notification interval(in minutes)",
+    }
+  ],
 
   execute: async ctx => {
     if (honami.reminders.has(ReminderTypes.BURN)) {
       return
     }
 
-    await ctx.respondInteraction({ content: "にゃ～～ん！！" })
+    await ctx.respondInteraction({ content: Messages.Bonus.Start })
 
-    const r = new Reminder(ReminderTypes.BURN)
-    const ch = new Channel(honami.listenOnlyChannelID)
-    r.start(5, async () => await ch.send({ content: "<:emoji_14:1176498845333598269>" }))
-    honami.reminders.set(ReminderTypes.BURN, r)
+    const interval = ctx.getOption<number>("interval") ?? 15
+    const reminder = new Reminder(ReminderTypes.BURN)
+    const channel = new Channel(honami.listenOnlyChannelID)
+    reminder.start(
+      interval * 1000,
+      async () => await channel.send({ content: "<:emoji_14:1176498845333598269>" })
+    )
+    honami.reminders.set(ReminderTypes.BURN, reminder)
   }
 })
