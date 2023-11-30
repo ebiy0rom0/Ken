b64e408aeda690703d410d7afd73dbe06cca8058
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-11-30 17:09:01 +0900

    [add]message getter

--- src/structures/discord/channel.ts
+++ src/structures/discord/channel.ts
@@ -11,6 +11,7 @@ export class Channel {
     if ((await guild.channels()).has(this.id)) throw Error(`Channel is not exists: ${this.id}`)
   }
 
+  messages = async () => await honami.helpers.getMessages(this.id)
   send = async (opt: CreateMessage) => await honami.helpers.sendMessage(this.id, opt)
   edit = async (opt: ModifyChannel) => await honami.helpers.editChannel(this.id, opt)
   delete = async (id: bigint) => await honami.helpers.deleteMessage(this.id, id)
