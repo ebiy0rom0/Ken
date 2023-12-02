3db26fcdca414bfb1655054fc004d2eb0ba864f5
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-03 02:04:21 +0900

    [wip]component interaction executor

--- src/structures/types/command.ts
+++ src/structures/types/command.ts
@@ -1,6 +1,8 @@
 import { CreateSlashApplicationCommand } from "../../deps.ts";
 import { ChatInputInteractionContext } from "../commands/chatInputInteractionContext.ts";
+import { ComponentInteractionContext } from "../commands/componentInteractionContext.ts";
 
 export interface ChatInputInteractionCommand extends Readonly<CreateSlashApplicationCommand> {
   readonly execute: (context: ChatInputInteractionContext) => Promise<unknown>
+  readonly componet?: (context: ComponentInteractionContext) => Promise<unknown>
 }
