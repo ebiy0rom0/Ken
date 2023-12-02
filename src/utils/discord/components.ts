f0ef66bf8a6fcf9781249fe6ea318f2adbbd4141
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-03 02:00:02 +0900

    [add]deteramination of interaction

--- src/utils/discord/components.ts
+++ src/utils/discord/components.ts
@@ -1,6 +1,9 @@
-import { ActionRow, InputTextComponent, MessageComponentTypes } from "../../deps.ts";
+import { ActionRow, InputTextComponent, Interaction, InteractionTypes, MessageComponentTypes } from "../../deps.ts";
 
 type OptionalProperty<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
+
+export const isComponentInteraction = (interaction: Interaction): boolean =>
+  interaction.type === InteractionTypes.MessageComponent || interaction.type === InteractionTypes.ModalSubmit
 
 export const createActionRow = (components: ActionRow["components"]): ActionRow => ({
   type: MessageComponentTypes.ActionRow,
