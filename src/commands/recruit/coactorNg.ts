b135530ca2c4dd2faeb4bbb98d621c26d025a0ce
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-03 02:02:34 +0900

    [add]coactor ng command

---
+++ src/commands/recruit/coactorNg.ts
@@ -0,0 +1,34 @@
+import { ken } from "../../client/ken.ts";
+import { TextStyles } from "../../deps.ts";
+import { Channel } from "../../structures/discord/channel.ts";
+import { createActionRow, createInputText } from "../../utils/discord/components.ts";
+import { MessageFlags } from "../../utils/discord/message.ts";
+import { createCommand } from "../../utils/mod.ts"
+
+export default createCommand({
+  name: "coactor-ng",
+  description: "Applying for a co-starring NG. This command is not visible to others.",
+
+  execute: async ctx => {
+    const ng = createInputText({
+      label: "NGの方",
+      customId: "ng",
+      style: TextStyles.Short,
+      required: true
+    })
+    await ctx.replyWithModal({
+      title: "共演NG 提出",
+      flags: MessageFlags.EPHEMERAL,
+      components: [createActionRow([ng])]
+    })
+  },
+
+  componet: async ctx => {
+    const ng = ctx.getOption<string>("ng")
+    await ctx.reply({
+      flags: MessageFlags.EPHEMERAL,
+      content: `${ng}`
+    })
+    await ken.botChannel.send({ content: `NG:${ctx.username} => ${ng}` })
+  }
+})
