c57f5d1a91848506a66a0ac51f848b459004e34e
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-03 02:07:26 +0900

    [mod]component interaction to be able to execute

--- src/events/handlers/interactionCreate.ts
+++ src/events/handlers/interactionCreate.ts
@@ -1,14 +1,37 @@
 import { ken } from "../../client/ken.ts"
+import { errorCommand } from "../../commands/error.ts";
+import { Interaction } from "../../deps.ts";
 import { ChatInputInteractionContext } from "../../structures/commands/chatInputInteractionContext.ts";
+import { ComponentInteractionContext } from "../../structures/commands/componentInteractionContext.ts";
+import { InteractionContext } from "../../structures/commands/interactionContext.ts";
+import { isComponentInteraction } from "../../utils/discord/components.ts";
+import { MessageFlags } from "../../utils/discord/message.ts";
 
 export const setInteractionCreate = () => {
   ken.events.interactionCreate = async (_, interaction) => {
-    const command = ken.commands.get(interaction.data?.name!)
-    const ctx = new ChatInputInteractionContext(interaction)
-    await command?.execute(ctx)
+    const ctx = await (isComponentInteraction(interaction) ?
+        executeComponentInteraction(interaction)
+      : executeChatInputInteraction(interaction))
 
-    if (!ctx.replied) {
-      ctx.sendReply({ content: "しゃ～！" })
-    }
+    await ctx.sendDeffer({
+      flags: MessageFlags.EPHEMERAL,
+      content: "へんじがないただのしかばねのようだ"
+    })
   }
+}
+
+const executeChatInputInteraction = async (interaction: Interaction): Promise<InteractionContext> => {
+  const ctx = new ChatInputInteractionContext(interaction)
+  const command = ken.commands.get(interaction.data?.name!) ?? errorCommand
+  await command?.execute(ctx)
+  return ctx
+}
+
+const executeComponentInteraction = async (interaction: Interaction): Promise<InteractionContext> => {
+  const ctx = new ComponentInteractionContext(interaction)
+  const command = ken.commands.get(interaction.data?.customId!) ?? errorCommand
+  if (command?.componet) {
+    await command?.componet(ctx)
+  }
+  return ctx
 }
