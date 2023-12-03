1b722d4267c2a73f5a46c65217fc190fb0be9f23
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-04 01:11:28 +0900

    [wip]voice channel participant check

--- src/structures/discord/channel.ts
+++ src/structures/discord/channel.ts
@@ -14,4 +14,15 @@ export class Channel {
   send = async (opt: CreateMessage) => await ken.helpers.sendMessage(this.id, opt)
   edit = async (opt: ModifyChannel) => await ken.helpers.editChannel(this.id, opt)
   delete = async (id: bigint) => await ken.helpers.deleteMessage(this.id, id)
+
+  private participantsCount = async (): Promise<number> => {
+    const participations = ken.kv.list<boolean>({ prefix: ["vc"] })
+    const count: boolean[] = []
+    for await (const entry of participations) {
+      count.push(entry.value)
+    }
+    return count.length
+  }
+  isFirstParticipant = async () => (await this.participantsCount()) === 1
+  isNoParticipant = async () => (await this.participantsCount()) === 0
 }
