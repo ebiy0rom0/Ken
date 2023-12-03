45a990b35c844e1c39844082feb77ef59bfa6916
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-04 01:10:48 +0900

    [mod]members returns extended member structure

--- src/structures/discord/guild.ts
+++ src/structures/discord/guild.ts
@@ -1,19 +1,31 @@
 import { ken } from "../../client/ken.ts";
+import { Collection } from "../../deps.ts";
 import { transformMember } from "../transformers/member.ts";
 import { DiscordMemberWithUser } from "../types/mod.ts";
 
 export class Guild {
   constructor(private id: bigint) {}
   channels = async () => await ken.helpers.getChannels(this.id)
-  members  = async () => await ken.helpers.getMembers(this.id, {})
-  member   = async (id: bigint) => {
+  members = async () => {
+    const results = await ken.rest.runMethod<DiscordMemberWithUser[]>(
+      ken.rest,
+      "GET",
+      ken.constants.routes.GUILD_MEMBERS(this.id, {}),
+    )
+
+    return new Collection(
+      results.map(result => {
+        const member = transformMember(result, this.id, ken.transformers.snowflake(result.user.id))
+        return [member.id, member];
+      }),
+    )
+  }
+  member = async (id: bigint) => {
     const result = await ken.rest.runMethod<DiscordMemberWithUser>(
       ken.rest,
       "GET",
       ken.constants.routes.GUILD_MEMBER(this.id, id),
-    );
-    console.log(result)
-
-    return await transformMember(result, this.id, id);
+    )
+    return await transformMember(result, this.id, id)
   }
 }
