89941c385b161b98ea87e56878cf40b8faa98c09
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-09 01:28:52 +0900

    [add]template replacer

--- src/config/messages.ts
+++ src/config/messages.ts
@@ -1,14 +1,25 @@
 
 export const Messages = {
-  Error: {
-
-  },
   Bonus: {
-    Start: "りはびり、しっかり炊けよ",
+    Start: "{0}、しっかり炊けよ",
     Stop: "今日はここまでだな"
   },
   Recruit: {
-    Start: "シフトの募集を開始する",
-    Stop: "シフトの募集を終了する"
+    NGReport: "共演NGを受付ました",
+    Start: "{0} のシフト募集を開始します",
+    Stop: "{0} のシフトの募集を終了します"
+  },
+  Error: {
+    ForgetReply: "へんじがないただのしかばねのようだ"
+  },
+  Debug: {
+
   }
+}
+
+// replace from {0} to args[0]
+// short hands "T"emplate replacement.
+export const T = (template: string, ...args: string[]): string => {
+  args.forEach((_, i) => template.replace(`{${i}}`, args[i]))
+  return template
 }
