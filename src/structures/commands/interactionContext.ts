444713c662d0c27dd6f3407a103d7b5f85e6bd00
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-08 16:53:17 +0900

    [mod]fix indent

--- src/structures/commands/interactionContext.ts
+++ src/structures/commands/interactionContext.ts
@@ -2,30 +2,37 @@ import { ken } from "../../client/ken.ts";
 import { Interaction, InteractionCallbackData, InteractionResponseTypes } from "../../deps.ts";
 
 export class InteractionContext {
-    replied = false
-    constructor(protected interaction: Interaction) {}
+  replied = false
+  constructor(protected interaction: Interaction) {}
 
-    get userID () { return this.interaction.user.id }
-    get username () { return this.interaction.user.username }
+  get userID () { return this.interaction.user.id }
+  get username () { return this.interaction.user.username }
 
-    getOption = <T>(name: string): T | undefined => {
-      const options = this.interaction.data?.options ?? []
-      return options.find(opt => opt.name === name)?.value as T
-    }
+  getOption = <T>(name: string): T | undefined => {
+    const options = this.interaction.data?.options ?? []
+    return options.find(opt => opt.name === name)?.value as T
+  }
 
-    reply = async (options: InteractionCallbackData) => {
+  reply = async (options: InteractionCallbackData) => {
+    if (!this.replied) {
       this.replied = true
       await ken.helpers.sendInteractionResponse(this.interaction.id, this.interaction.token, {
         type: InteractionResponseTypes.ChannelMessageWithSource,
         data: options
       })
-    }
-
-    sendDeffer = async (options: InteractionCallbackData) => {
-      if (this.replied) return
+    } else {
       await ken.helpers.sendInteractionResponse(this.interaction.id, this.interaction.token, {
-        type: InteractionResponseTypes.ChannelMessageWithSource,
+        type: InteractionResponseTypes.DeferredUpdateMessage,
         data: options
       })
     }
   }
+
+  sendDeffer = async (options: InteractionCallbackData) => {
+    if (this.replied) return
+    await ken.helpers.sendInteractionResponse(this.interaction.id, this.interaction.token, {
+      type: InteractionResponseTypes.ChannelMessageWithSource,
+      data: options
+    })
+  }
+}
