2e3d8917f2816262d5a4dd6d2bd24fd155681cf0
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-09 02:11:00 +0900

    [wip]auto reminder start
    trigger by entry voice channel

--- src/events/handlers/voiceStateUpdate.ts
+++ src/events/handlers/voiceStateUpdate.ts
@@ -1,6 +1,8 @@
 import { ken } from "../../client/ken.ts";
 import { Config } from "../../config/config.ts";
+import { Channel } from "../../structures/discord/channel.ts";
 import { VoiceChannel } from "../../structures/discord/voiceChannel.ts";
+import { Reminder, ReminderTypes } from "../../utils/mod.ts";
 
 export const setVoiceStateUpdate = () => {
   ken.events.voiceStateUpdate = async (_, voiceState) => {
@@ -22,13 +24,21 @@ export const setVoiceStateUpdate = () => {
 
       await vc.entry(member.id)
       if (await vc.isFirstEntry()) {
-        ken.botChannel.send({ content: `営業開始 DA☆` })
+        await ken.botChannel.send({ content: `炊き忘れ防止リマインダー 起動` })
+
+        const reminder = new Reminder(ReminderTypes.BURN)
+        const channel  = new Channel(ken.transformers.snowflake(Config.LISTEN_ONLY_CHANNEL_ID))
+        reminder.start(
+          5 * 1000,
+          async () => await channel.send({ content: "<:emoji_14:1176498845333598269>" })
+        )
       }
 
     } else {
       await vc.exit(member.id)
       if (await vc.isEmpty()) {
-        await ken.botChannel.send({ content: `営業終了 DA☆` })
+        await ken.botChannel.send({ content: `炊き忘れ防止リマインダー 停止` })
+        ken.reminders.get(ReminderTypes.BURN)?.stop()
       }
     }
   }
