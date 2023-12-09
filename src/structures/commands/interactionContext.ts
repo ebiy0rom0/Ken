453f3de288c18e6a84c58782895511ad27d502bc
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-10 03:27:54 +0900

    [mod]unstable: edit original response

--- src/structures/commands/interactionContext.ts
+++ src/structures/commands/interactionContext.ts
@@ -1,6 +1,9 @@
 import { ken } from "../../client/ken.ts";
-import { Interaction, InteractionCallbackData, InteractionResponseTypes } from "../../deps.ts";
-import { MessageFlags } from "../../utils/mod.ts";
+import {
+  Interaction,
+  InteractionCallbackData,
+  InteractionResponseTypes
+} from "../../deps.ts";
 
 export class InteractionContext {
   replied = false
@@ -8,6 +11,9 @@ export class InteractionContext {
 
   get userID () { return this.interaction.user.id }
   get username () { return this.interaction.user.username }
+
+  saveToken = async (key: string, expireIn: number = 3 * 60 * 1000) =>
+    await ken.kv.set(["interaction", key], this.interaction.token, { expireIn })
 
   getOption = <T>(name: string): T | undefined => {
     const options = this.interaction.data?.options ?? []
@@ -42,5 +48,15 @@ export class InteractionContext {
       type: InteractionResponseTypes.ChannelMessageWithSource,
       data: options
     })
+  }
+
+  editOriginalResponse = async (key: string, options: InteractionCallbackData) => {
+    const token = await ken.kv.get<string>(["interaction", key])
+    if (token.value === null) return
+
+    await ken.helpers.editOriginalInteractionResponse(
+      token.value!,
+      options
+    )
   }
 }
