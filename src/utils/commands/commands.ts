e22bf559de0ac3d222fe6c94fe9b1401453a45f8
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-10 21:04:24 +0900

    [mod]fix autoloader
    load to static moduler

--- src/utils/commands/commands.ts
+++ src/utils/commands/commands.ts
@@ -1,33 +1,15 @@
-import { fs, path } from "../../deps.ts";
-import { ChatInputInteractionCommand } from "../../structures/types/mod.ts";
 import { ken } from "../../client/ken.ts";
 import { Config } from "../../config/config.ts";
-
-const collect = async <T>(iter: AsyncIterable<T>): Promise<T[]> => {
-  const results: T[] = []
-
-  for await (const result of iter) {
-      results.push(result)
-  }
-
-  return results
-}
-
-const resourceDir = path.resolve(`src/commands/`)
-export const commands = await collect(fs.walk(resourceDir, { includeDirs: false }))
+import { modules } from "../../commands/mod.ts";
 
 export const loadCommands = async (): Promise<void> => {
   try {
-    await Promise.all(commands.map(file => file.path).filter(file => path.extname(file) === ".ts").map(async path => {
-      const command = (await import(path))
-      console.log(command)
-      const cmd = command.default as ChatInputInteractionCommand
-      ken.commands.set(command.name, command)
-    }))
+    await Promise.all(modules.map(async command =>
+      await ken.commands.set(command.name, command)
+    ))
   } catch (error) {
     console.log(`Error loading command: ${error}`)
   }
-
   ken.commands.size > 0 ? upsertApplicationCommands() : console.log("No commands")
 }
 
@@ -41,5 +23,3 @@ const upsertApplicationCommands = async () => {
     console.log(`Error while registing commands: ${error}`)
   }
 }
-
-export const createCommand = (command: ChatInputInteractionCommand) => command
