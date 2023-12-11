27d492d9a38e4c05ff5d0c03c625b3721ee77160
ebiy0rom0 <kk.n0h2a2l3.end@gmail.com>
2023-12-11 23:55:52 +0900

    [mod]token loads direct

--- src/client/ken.ts
+++ src/client/ken.ts
@@ -1,8 +1,7 @@
-import { Config } from "../config/config.ts";
 import { Intents, createBot } from "../deps.ts";
 import { Ken } from "../structures/types/mod.ts";
 
 export const ken = createBot({
-  token: Config.DISCORD_TOKEN,
+  token: Deno.env.get("DISCORD_TOKEN")!,
   intents: Intents.Guilds | Intents.GuildMessages | Intents.MessageContent | Intents.GuildVoiceStates,
 }) as Ken
